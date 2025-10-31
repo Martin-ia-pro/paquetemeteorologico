aece5dd64e5d1f179145a27c687bab4be0aa0a33
Juan Martin Bongiovanni <jmbongiovanni@mail.austral.edu.ar>
2025-10-31 18:08:14 -0300

    Crear datos y su descripcion provisoria

---
+++ R/NH0437.R
@@ -0,0 +1,10 @@
+#' Datos estación NH0437
+#'
+#' Set de datos que contiene la información sobre la estacion NH0437
+#'
+#' @format vector.
+#'
+#' \describe{
+#' \item{datos}{númerico, de largo 1000.}
+#'}
+"NH0437"
---
+++ R/NH0910.R
@@ -0,0 +1,10 @@
+#' Datos estación NH0910
+#'
+#' Set de datos que contiene la información sobre la estacion NH0910
+#'
+#' @format vector.
+#'
+#' \describe{
+#' \item{datos}{númerico, de largo 1000.}
+#'}
+"NH0910"
