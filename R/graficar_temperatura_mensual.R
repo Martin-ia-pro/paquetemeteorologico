6b535f1dc21e38b68791ff9a94ac5e1117dbb3f3
LisandroSeghezzo2003 <seghezzolisandro03@gmail.com>
2025-11-05 20:12:02 -0300

    Modifique la documentacion

--- R/graficar_temperatura_mensual.R
+++ R/graficar_temperatura_mensual.R
@@ -1,6 +1,6 @@
 #' Gráfico de Líneas de Temperatura Promedio Mensual
 #'
-#' Genera un gráfico de líneas (ggplot2) que visualiza la temperatura promedio
+#' Genera un gráfico de líneas (ggplot2) que permite visualizar la evolucion de la temperatura promedio
 #' mensual a partir del conjunto de datos de estaciones meteorológicas proporcionados.
 #'
 #' Esta función agrupa los datos por estación (`id`) y mes, calcula el promedio
@@ -21,13 +21,13 @@
 #' @param titulo La cadena de texto que se desea utilizar como
 #'   título principal del gráfico generado.Por defecto, es "Temperatura Mensual".
 #'
-#' @return Un objeto de la clase `ggplot` que representa
-#'   el gráfico de líneas de la temperatura promedio mensual. Este objeto puede
-#'   ser impreso o guardado.
+#' @return Un objeto de la clase `ggplot` que representa el gráfico de líneas de la temperatura promedio mensual.
+#' Este objeto puede ser impreso o guardado.
 #'
 #' @examples
 #' graficar_temperatura_mensual(NH0437,
-#'                              c("green", "blue", "red","yellow","purple"),"Temperatura Promedio Mensual Estación NH0437")
+#'                              c("green", "blue", "red","yellow","purple"),
+#'                              "Temperatura Promedio Mensual Estación NH0437")
 #' graficar_temperatura_mensual(NH0910, ,"Temperatura Promedio Mensual Estación NH0910")
 #' #se puede usar sin poner colores
 #'
